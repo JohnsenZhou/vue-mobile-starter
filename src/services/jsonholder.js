7eeaab32ba7c5ab579c952c5d918c9cd952c0a19
Johnsen <296240671@qq.com>
2017-08-04 16:07:05 +0800

    update api server

--- src/services/jsonholder.js
+++ src/services/jsonholder.js
@@ -2,48 +2,48 @@ import request from '../utils/request';
 
 
 export async function getPostsList() {
-  return request('/replace/posts');
+  return await request('/replace/posts');
 }
 
 export async function getPostDetail(id) {
-  return request(`/replace/posts/${id}`);
+  return await request(`/replace/posts/${id}`);
 }
 
 export async function updatePostDetail(values) {
-  return request(`/replace/posts/${values.id}`, {
+  return await request(`/replace/posts/${values.id}`, {
     method: 'PUT',
     data: values,
   });
 }
 
 export async function getPostComment(id) {
-  return request(`/replace/posts/${id}/comments`);
+  return await request(`/replace/posts/${id}/comments`);
 }
 
 export async function getUsers() {
-  return request('/replace/users');
+  return await request('/replace/users');
 }
 
 export async function getUserDetail(userId) {
-  return request(`/replace/users/${userId}`);
+  return await request(`/replace/users/${userId}`);
 }
 
 export async function getUserTodoList(userId) {
-  return request(`/replace/users/${userId}/todos`);
+  return await request(`/replace/users/${userId}/todos`);
 }
 
 export async function getUserPostList(userId) {
-  return request(`/replace/users/${userId}/posts`);
+  return await request(`/replace/users/${userId}/posts`);
 }
 
 export async function getUserAlbumsList(userId) {
-  return request(`/replace/users/${userId}/albums`);
+  return await request(`/replace/users/${userId}/albums`);
 }
 
 export async function getAlbums() {
-  return request('/replace/albums');
+  return await request('/replace/albums');
 }
 
 export async function getAlbumDetail(id) {
-  return request(`/replace/albums/${id}/photos`);
+  return await request(`/replace/albums/${id}/photos`);
 }
