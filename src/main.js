fa3afd0608762185baf463a95ac22a5b7b6a2c7f
Johnsen <296240671@qq.com>
2017-09-21 15:09:24 +0800

    shrink remove && add img lazyload

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue';
 import ClipLoader from 'vue-spinner/src/ClipLoader.vue';
 import MuseUI from 'muse-ui';
+import VueLazyload from 'vue-lazyload';
 import 'muse-ui/dist/muse-ui.css';
 import 'muse-ui/dist/theme-light.css'
 import App from './App';
@@ -13,8 +14,11 @@ import './assets/css/common.css';
 
 Vue.config.productionTip = false;
 Vue.component('clip-loader', ClipLoader);
-// 引入museUI
+// 引入museUI && lazyload
 Vue.use(MuseUI);
+Vue.use(VueLazyload, {
+  loading: 'static/load.gif'
+});
 
 /* eslint-disable no-new */
 new Vue({
